fde407c95373ff1553ac3c5adfa273330c7ce518
Dev-Ben-Theo <theophilusben00@gmail.com>
2026-07-24 17:21:07 +0000

    fixed issues

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { App } from './App';
+import { NotFound } from './pages/NotFound';
+import { ToastProvider } from './components/Toast';
 import './app/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ToastProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </ToastProvider>
   </React.StrictMode>,
 );
